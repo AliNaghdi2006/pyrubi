49b4c04fb651ea1de640a0334779e657d2b9a95e
Ali <AliNaghdi2006@gmail.com>
2025-01-14 00:36:03 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name = "pyrubi",
     version = "3.6.0",
-    author="Ali Ganji zadeh",
-    author_email = "ali.ganji.za@gmail.com",
+    author="Ali Naghdi",
+    author_email = "alinaghdi2006@gmail.com",
     description = "This is a powerful and easy library for building self bot in the Rubika.",
     keywords = ["rubika", "rubino", "pyrubi", "pyrubika", "bot", "chat"],
     long_description = open("README.md", encoding="utf-8").read(),
     python_requires="~=3.7",
     long_description_content_type = "text/markdown",
-    url = "https://github.com/AliGanji1/pyrubi",
+    url = "https://github.com/AliNaghdi2006/pyrubi",
     packages = find_packages(),
     install_requires = ["urllib3", "tqdm", "websocket-client", "pycryptodome", "mutagen", "filetype"],
     classifiers = [
